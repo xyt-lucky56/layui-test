31f0bccd262a0d9ed8f3649d743dc11657acb177
liujing <1394624748@qq.com>
2019-09-09 17:57:20 +0800

    2

--- src/api/api.js
+++ src/api/api.js
@@ -1,23 +1,5 @@
-import https from '@/utils/axios'  //引入我们二次封装的axios.js文件
- 
+import https from '@/utils/axios'  //引入我们二次封装的axios.js文件 
 
-export const MYTRANFERRECORD = 'transfer/myTranferRecord'
-
-export const  myTranferRecord = function(params){
-    return https(MYTRANFERRECORD, params);
-}
-
-export const SYSTEMRECORD = 'user/systemRecord'
-
-export const  systemRecord = function(params){
-    return https(SYSTEMRECORD, params,'get');
-}
-
-export const INFO = 'info/myTranferRecord'
-
-export const  roleInfo = function(params){
-    return https(INFO, params,'get');
-}
 
 export const QUERYSYSTEMINFO = '/system/querySystemInfo'
 export const querySystemInfo = function(params){
